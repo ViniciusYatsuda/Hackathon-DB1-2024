e28fd5d4484569b23ae87d6692ef660cdf245227
ViniciusYatsuda <viniyatsuda@gmail.comdagit config --global user.email viniyatsuda@gmail.com>
2024-05-29 14:42:14 -0300

    Realizei a criação do endpoint que busca todos os itens dentro do bando de dados, tambem como realizei a conexão do back-end com o front-end atraves do Axios

--- Back-End/routes/users.js
+++ Back-End/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const { Product, sequelize} = require('../db.js')
+
+//Rota para postagem de mais item
+router.post('/',async (request,response)=>{
+  try{
+    const body = request.body;
+    let user = await Product.create(body);
+    response.status(201).json(user);
+  }catch(err){
+  console.log(err);
+  response.status(400).send('Falha ao consultar os usuarios')
+}})
+
+router.get('/',async (request,response)=>{
+  try{
+    const body = await Product.findAll();
+    response.status(201).json(body);
+  }catch(err){
+  console.log(err);
+  response.status(400).send('Falha ao consultar os usuarios')
+}})
+
+
 
 module.exports = router;
--- Front-End/src/lojaGeral.jsx
+++ Front-End/src/lojaGeral.jsx
@@ -5,6 +5,8 @@ import {Switch} from 'antd';
 import { Carousel } from 'antd';
 import { Card, Col, Row } from 'antd';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
+import { useState,useEffect } from 'react';
 
 const contentStyle = {
   height: '200px',
@@ -14,132 +16,147 @@ const contentStyle = {
   background: '#364d79',
 };
 
-const products = [
-  {
-    id: 1,
-    name: 'Tênis Nike',
-    description: 'Confortável e estiloso.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-nike.jpg',
-  },
-  {
-    id: 2,
-    name: 'Tênis Adidas',
-    description: 'Design moderno e elegante.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-adidas.jpg',
-  },
-  {
-    id: 3,
-    name: 'Tênis Puma',
-    description: 'Leve e confortável.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-puma.jpg',
-  },
-  {
-    id: 4,
-    name: 'Tênis Reebok',
-    description: 'Ideal para corridas.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-reebok.jpg',
-  },
-  {
-    id: 5,
-    name: 'Tênis Asics',
-    description: 'Alta performance e durabilidade.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-asics.jpg',
-  },
-  {
-    id: 6,
-    name: 'Tênis Mizuno',
-    description: 'Conforto e estabilidade.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-mizuno.jpg',
-  },
-  {
-    id: 7,
-    name: 'Tênis New Balance',
-    description: 'Estilo clássico e conforto.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-new-balance.jpg',
-  },
-  {
-    id: 8,
-    name: 'Tênis Under Armour',
-    description: 'Tecnologia avançada para desempenho.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-under-armour.jpg',
-  },
-  {
-    id: 9,
-    name: 'Tênis Skechers',
-    description: 'Conforto e estilo para o dia a dia.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-skechers.jpg',
-  },
-  {
-    id: 10,
-    name: 'Tênis Fila',
-    description: 'Design moderno e esportivo.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-fila.jpg',
-  },
-  {
-    id: 11,
-    name: 'Tênis Oakley',
-    description: 'Resistência e durabilidade.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-oakley.jpg',
-  },
-  {
-    id: 12,
-    name: 'Tênis Converse',
-    description: 'Clássico e versátil.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-converse.jpg',
-  },
-  {
-    id: 13,
-    name: 'Tênis Vans',
-    description: 'Estilo skatista e conforto.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-vans.jpg',
-  },
-  {
-    id: 14,
-    name: 'Tênis Hoka One One',
-    description: 'Ideal para longas distâncias.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-hoka-one-one.jpg',
-  },
-  {
-    id: 15,
-    name: 'Tênis Brooks',
-    description: 'Alta performance para corredores.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-brooks.jpg',
-  },
-  {
-    id: 16,
-    name: 'Tênis Salomon',
-    description: 'Ideal para trilhas e aventuras.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-salomon.jpg',
-  },
-  {
-    id: 17,
-    name: 'Tênis Saucony',
-    description: 'Estabilidade e conforto.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-saucony.jpg',
-  },
-  {
-    id: 18,
-    name: 'Tênis Merrell',
-    description: 'Durabilidade e conforto para trilhas.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-merrell.jpg',
-  },
-  {
-    id: 19,
-    name: 'Tênis Altra',
-    description: 'Design inovador e conforto.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-altra.jpg',
-  },
-  {
-    id: 20,
-    name: 'Tênis La Sportiva',
-    description: 'Ideal para atividades ao ar livre.',
-    image: 'https://link-para-imagem-do-produto.com/tenis-la-sportiva.jpg',
-  }
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Tênis Nike',
+//     description: 'Confortável e estiloso.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-nike.jpg',
+//   },
+//   {
+//     id: 2,
+//     name: 'Tênis Adidas',
+//     description: 'Design moderno e elegante.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-adidas.jpg',
+//   },
+//   {
+//     id: 3,
+//     name: 'Tênis Puma',
+//     description: 'Leve e confortável.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-puma.jpg',
+//   },
+//   {
+//     id: 4,
+//     name: 'Tênis Reebok',
+//     description: 'Ideal para corridas.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-reebok.jpg',
+//   },
+//   {
+//     id: 5,
+//     name: 'Tênis Asics',
+//     description: 'Alta performance e durabilidade.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-asics.jpg',
+//   },
+//   {
+//     id: 6,
+//     name: 'Tênis Mizuno',
+//     description: 'Conforto e estabilidade.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-mizuno.jpg',
+//   },
+//   {
+//     id: 7,
+//     name: 'Tênis New Balance',
+//     description: 'Estilo clássico e conforto.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-new-balance.jpg',
+//   },
+//   {
+//     id: 8,
+//     name: 'Tênis Under Armour',
+//     description: 'Tecnologia avançada para desempenho.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-under-armour.jpg',
+//   },
+//   {
+//     id: 9,
+//     name: 'Tênis Skechers',
+//     description: 'Conforto e estilo para o dia a dia.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-skechers.jpg',
+//   },
+//   {
+//     id: 10,
+//     name: 'Tênis Fila',
+//     description: 'Design moderno e esportivo.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-fila.jpg',
+//   },
+//   {
+//     id: 11,
+//     name: 'Tênis Oakley',
+//     description: 'Resistência e durabilidade.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-oakley.jpg',
+//   },
+//   {
+//     id: 12,
+//     name: 'Tênis Converse',
+//     description: 'Clássico e versátil.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-converse.jpg',
+//   },
+//   {
+//     id: 13,
+//     name: 'Tênis Vans',
+//     description: 'Estilo skatista e conforto.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-vans.jpg',
+//   },
+//   {
+//     id: 14,
+//     name: 'Tênis Hoka One One',
+//     description: 'Ideal para longas distâncias.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-hoka-one-one.jpg',
+//   },
+//   {
+//     id: 15,
+//     name: 'Tênis Brooks',
+//     description: 'Alta performance para corredores.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-brooks.jpg',
+//   },
+//   {
+//     id: 16,
+//     name: 'Tênis Salomon',
+//     description: 'Ideal para trilhas e aventuras.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-salomon.jpg',
+//   },
+//   {
+//     id: 17,
+//     name: 'Tênis Saucony',
+//     description: 'Estabilidade e conforto.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-saucony.jpg',
+//   },
+//   {
+//     id: 18,
+//     name: 'Tênis Merrell',
+//     description: 'Durabilidade e conforto para trilhas.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-merrell.jpg',
+//   },
+//   {
+//     id: 19,
+//     name: 'Tênis Altra',
+//     description: 'Design inovador e conforto.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-altra.jpg',
+//   },
+//   {
+//     id: 20,
+//     name: 'Tênis La Sportiva',
+//     description: 'Ideal para atividades ao ar livre.',
+//     image: 'https://link-para-imagem-do-produto.com/tenis-la-sportiva.jpg',
+//   }
+// ];
 
 const { Meta } = Card;
 
 function About() {
+  
+    const [Product, setProduct] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('http://localhost:3000/users');
+          setProduct(response.data);
+        } catch (error) {
+          console.error('Erro ao buscar itens do carrossel:', error);
+        }
+      };
+      fetchData();
+    }, []);
+  
   return (
     <div className="slide-up">
       <header>
@@ -176,13 +193,13 @@ function About() {
             <h2>TENIS</h2>
             <div style={{ padding: '30px' }}>
               <Row gutter={[16, 16]}>
-                {products.map(product => (
+                {Product.map(product => (
                   <Col key={product.id} xs={24} sm={12} md={8} lg={6}>
                     <Card
                         hoverable
                         cover={<img alt={product.name} src={product.image} />}
                       >
-                      <Meta title={product.name} description={product.description} />
+                      <Meta title={product.produto} description={product.price} />
                     </Card>
                   </Col>
                 ))}
