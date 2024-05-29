81ffc0690b9c62558e010ea36f8f654d6492fa1a
ViniciusYatsuda <viniyatsuda@gmail.comdagit config --global user.email viniyatsuda@gmail.com>
2024-05-29 00:51:32 -0300

    Criei a conexão com o banco de dados, com os arquivos necessarios do Dockers, do PostgresSQL e usando o ORM Sequelize para a conexão.

---
+++ Back-End/db.js
@@ -0,0 +1,43 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    'hackathon',// db
+    'postgres',//user
+    '123',// senha
+    {
+        dialect:'postgres',
+        host:'localhost',
+        port:'5432',
+        define:{
+            timestamps:false
+        }
+    }
+)
+
+const Product = sequelize.define('produto',{
+    id:{
+        type: Sequelize.DataTypes.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement:true,
+    },
+    produto:{
+        type:Sequelize.DataTypes.STRING(255),
+        allowNull:false,
+    },
+    categoria:{
+        type:Sequelize.DataTypes.STRING(100),
+        allowNull:true,
+        deffaultValue:null,
+    },
+    price:{
+        type:Sequelize.DataTypes.FLOAT,
+        allowNull:false,
+        defaultValue:null,
+    }
+});
+
+module.exports = {
+    sequelize,
+    Product
+};
